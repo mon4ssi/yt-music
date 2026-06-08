ac0d9fa13f8c65d87f19ad6e9326d8fac29a3809
Monassi Carrasco <mon4ssi@outlook.com>
2026-06-08 15:57:32 -0500

    feat: harden Tauri capabilities, IPC, and WebView security

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import { useEffect } from 'react'
 import './App.css'
 
-const YT_MUSIC_URL = 'https://music.youtube.com'
-
 function App() {
-  useEffect(() => {
-    const isTauri = '__TAURI_INTERNALS__' in window
-    if (isTauri) {
-      window.location.href = YT_MUSIC_URL
-    }
-  }, [])
-
-  return (
-    <div className="app-splash">
-      <p className="app-splash-text">Loading YouTube Music...</p>
-    </div>
-  )
+  return null
 }
 
 export default App
